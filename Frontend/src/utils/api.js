455f4eb11825971364ea6c620ab74068e2070af8
byteRaj-commits <rajp182138@gmail.com>
2026-04-13 19:12:08 +0530

    ready for deployment

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
-const API = import.meta.env.VITE_API_URL;
-
 const api = axios.create({
- 
-  baseURL: "https://personal-portfolio-81xi.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
